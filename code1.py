9c29c7a0382bc4433a9c81d0e7a8d00599ddbb08
XeNn7012 <99806271+XeNn7012@users.noreply.github.com>
2022-02-16 17:24:49 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,11 @@
+y=[]       
+x=[1,2,3,2,3,5,6]
+for i in range(len(x)):
+    for j in range(len(x)):
+        try:
+            if x[i]==x[i+j+1]:
+                y.append(x[i])
+                
+        except IndexError:
+            pass
+print(y.pop(0))
